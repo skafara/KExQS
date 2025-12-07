0899a175d4a281363ac9431fc12d4f45bbb97fba
Stanislav Kafara <skafara@students.zcu.cz>
2025-12-07 15:02:15 +0100

    better setup/build system

--- Build.bat
+++ Build.bat
@@ -1,12 +1,35 @@
 @echo off
 
+REM Ensure obj/bin directories exist
+
 if not exist obj mkdir obj
 if not exist bin mkdir bin
+
+REM Delete previous build files
 
 del /q /s "obj\*"
 del /q /s "bin\*"
 
-@echo on
-cl /std:c++20 /EHsc /LD /Foobj\ /Febin\ESimulator.dll /I src\cpp\include src\cpp\lib\**.cpp /I %OPENCL_SDK_PATH%\include /D CL_HPP_TARGET_OPENCL_VERSION=200 /D OPENCL_KERNELS_PATH=\"%OPENCL_KERNELS_PATH%\" /link /LIBPATH:%OPENCL_SDK_PATH%\lib OpenCL.lib
+REM Build C++ DLL
+
+cl  /std:c++20 /EHsc /LD /Foobj\ /Febin\ESimulator.dll ^
+    /I src\cpp\include ^
+    src\cpp\lib\**.cpp ^
+    /I %TBB_INCLUDE% ^
+    /I %OPENCL_INCLUDE% ^
+    /D CL_HPP_TARGET_OPENCL_VERSION=200 ^
+    /D OPENCL_KERNELS_PATH=\"%OPENCL_KERNELS_PATH%\" ^
+    /link ^
+    /LIBPATH:%TBB_LIB% ^
+    /LIBPATH:%OPENCL_LIB% ^
+    tbb12.lib ^
+    OpenCL.lib
+
+REM Copy required runtime files
+
+copy %TBB_REDIST%\tbb12.dll bin
 xcopy /Y /E /I src\cpp\kernels bin\%OPENCL_KERNELS_PATH%
+
+REM Build Pascal Application
+
 fpc -Px86_64 -dRUN_EXTERNAL -FEbin -FUobj -Foobj -Fusrc\pascal\lib src\pascal\program\KExQS_Test01.lpr
--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,46 @@
 @echo off
 
+REM Libraries
+
+REM Create ext directory
+
+if not exist ext mkdir ext
+
+
+REM oneTBB
+
+set TBB_VERSION=2022.3.0
+set TBB_URL=https://github.com/oneapi-src/oneTBB/releases/download/v%TBB_VERSION%/oneapi-tbb-%TBB_VERSION%-win.zip
+set TBB_ZIP=ext\oneapi-tbb-%TBB_VERSION%-win.zip
+set TBB_DIR=ext\oneTBB
+
+if not exist %TBB_DIR% (
+    powershell -Command "& {Invoke-WebRequest '%TBB_URL%' -OutFile '%TBB_ZIP%'}"
+    powershell -Command "& {Expand-Archive '%TBB_ZIP%' 'ext'}"
+    del %TBB_ZIP%
+    for /d %%D in ("ext\oneapi-tbb-%TBB_VERSION%*") do (
+        ren "%%D" oneTBB
+    )
+)
+
+REM OpenCL
+
+set OPENCL_VERSION=2025.07.23
+set OPENCL_URL=https://github.com/KhronosGroup/OpenCL-SDK/releases/download/v%OPENCL_VERSION%/OpenCL-SDK-v%OPENCL_VERSION%-Win-x64.zip
+set OPENCL_ZIP=ext\OpenCL-SDK-v%OPENCL_VERSION%-Win-x64.zip
+set OPENCL_DIR=ext\OpenCL
+
+if not exist %OPENCL_DIR% (
+    powershell -Command "& {Invoke-WebRequest '%OPENCL_URL%' -OutFile '%OPENCL_ZIP%'}"
+    powershell -Command "& {Expand-Archive '%OPENCL_ZIP%' 'ext'}"
+    del %OPENCL_ZIP%
+    for /d %%D in ("ext\OpenCL-SDK-v%OPENCL_VERSION%*") do (
+        ren "%%D" OpenCL
+    )
+)
+
+REM Visual Studio Environment
+
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath`) do (
     set VS_PATH=%%i
 )
@@ -9,7 +50,20 @@ if not defined VS_PATH (
     exit /b 1
 )
 
+REM Set up environment variables
+
 call "%VS_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
-set OPENCL_SDK_PATH=ext\OpenCL-SDK-v2025.07.23-Win-x64
+
+REM Project Environment Variables
+
+set TBB_INCLUDE=%TBB_DIR%\include
+set TBB_LIB=%TBB_DIR%\lib\intel64\vc14
+set TBB_REDIST=%TBB_DIR%\redist\intel64\vc14
+
+set OPENCL_INCLUDE=%OPENCL_DIR%\include
+set OPENCL_LIB=%OPENCL_DIR%\lib
+
 set OPENCL_KERNELS_PATH=kernels
+
+REM Done
 cmd
